a7185e8baa92a844f44d4e0ebcb7b2d319e9c198
BluesKutya <33314772+BluesKutya@users.noreply.github.com>
2022-08-22 05:25:53 +0200

    add solution and freecoffee plugin

---
+++ FreeCoffee/FreeCoffee.cs
@@ -0,0 +1,27 @@
+﻿using HarmonyLib;
+using PixelCrushers.DialogueSystem.PlayMaker;
+
+namespace FreeCoffee
+{
+	class FreeCoffee
+	{
+
+		[HarmonyPatch(typeof(GetVariable))]
+		[HarmonyPatch(nameof(GetVariable.GetAndStore))]
+		internal class PlayMaker_GetVariable__GetAndStore__Patch
+		{
+			[HarmonyPrefix]
+			public static bool Prefix(GetVariable __instance)
+			{
+				if (__instance.variableName == null || string.IsNullOrEmpty(__instance.variableName.Value) || !"Player_Energy".Equals(__instance.variableName.Value)) {
+					return true; //run the orignal code
+				}
+
+				__instance.storeFloatResult.Value = 100.0f;
+				return false; //Tell Harmony to not run the original method
+			}
+		}
+
+	}
+
+}
---
+++ FreeCoffee/Plugin.cs
@@ -0,0 +1,24 @@
+﻿using System.Reflection;
+using HarmonyLib;
+using BepInEx;
+
+namespace FreeCoffee
+{
+
+    [BepInPlugin("blueskutya.citizen-sleeper.plugin", "free-coffee", "1.0.0")]
+    public class Plugin : BaseUnityPlugin
+    {
+        private void Awake()
+        {
+            var harmony = new Harmony("blueskutya.citizensleeper.freecoffee.patch");
+
+            var assembly = Assembly.GetExecutingAssembly();
+            harmony.PatchAll(assembly);
+
+            // Plugin startup logic
+            Logger.LogInfo("Plugin free-coffee is loaded!");
+        }
+
+    }
+
+}
